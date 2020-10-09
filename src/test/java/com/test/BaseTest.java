d7a9079413582ccfee8346c90a8570f1ba476f50
unknown <Chitresh007Z>
2020-10-09 12:47:03 +0530

    pom.xml modified, extent and allure reports added

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -9,9 +9,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.pages.BasePage;
 import com.pages.Page;
 import com.pom.pages.FilterPage;
@@ -27,13 +31,38 @@ public class BaseTest {
 	WebDriver driver;
 	public Page page;
 	List<String> name=null;
-	
+	ExtentReports extent;
+
+	@BeforeSuite
+	public void config()
+	{
+		String path =System.getProperty("user.dir")+"\\Reports\\index.html";
+
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+
+		reporter.config().setReportName("Web Automation Results");
+
+		reporter.config().setDocumentTitle("Test Results");
+
+		extent =new ExtentReports();
+
+		extent.attachReporter(reporter);
+		
+		extent.setSystemInfo("Tester", "Rajarshee Das");
+
+	}
+
 	@BeforeTest(description = "Opening Browser")
 	public void createDriver()
 	{
 		
+
+			ExtentTest test= extent.createTest("Create Driver");
+			
+
 			
 			String ChromePath = "C:\\Users\\user\\Desktop\\CTS_Onboarding\\Selenium Project\\Practo_AutomationFramework\\Chrome_Driver\\chromedriver.exe";
+
 			System.setProperty("webdriver.chrome.driver", ChromePath );
 		    driver=new ChromeDriver();
 			driver.manage().window().maximize();
@@ -47,62 +76,74 @@ public class BaseTest {
 			}
 			
 			page=new BasePage(driver);
-			
-			
-		
-		
+			extent.flush();
 	}
 	
 	@Test(priority=1)
-	public void verifyHomepagePageTest() {
+	public void verifyHomepagePageTest()
+	{
+		ExtentTest test= extent.createTest("Verify Home Page");
 		String title=page.getInstance(HomePage.class).getHomePageTItle();
 		String header=page.getInstance(HomePage.class).getHomePageHeader();
 		System.out.println("Title:-"+title);
 		System.out.println(header);
 		Assert.assertEquals(header, "How Practo is Helping India Fight COVID-19");
+		extent.flush();
 	}
 
 	
 	@Test(priority=2)
-	public void doLoginTest() {
-
+	public void doLoginTest()
+	{
+		ExtentTest test= extent.createTest("Login into account");
 		page.getInstance(loginPage.class).dologin("9903723035", "Ranjan@2020");
-		//page.getInstance(loginPage.class).dologin(" ", "");
 		String title=page.getInstance(loginPage.class).getLoginPageTitle();
 		Assert.assertEquals(title, "Practo | Video Consultation with Doctors, Book Doctor Appointments, Order Medicine, Diagnostic Tests");
+		extent.flush();
 	}
 	
 	@Test(priority=3)
-	public void searchtest(){
+	public void searchtest()
+	{
+		ExtentTest test= extent.createTest("Searching");
 		page.getInstance(SearchPage.class).dosearch("Hospital","Kolkata");
 		page.getInstance(SearchPage.class);
 		String SearchPageTitle=SearchPage.Searchtitle;
 		Assert.assertEquals(SearchPageTitle, "Best Hospitals in Kolkata - Book Appointment Online, View Fees, Reviews | Practo");
-		
+		extent.flush();
 	}
 	
 	@Test(priority=4)
-	public void FilterSearch() throws Exception{
+	public void FilterSearch() throws Exception
+	{
+		ExtentTest test= extent.createTest("Apply filters");
 		page.getInstance(FilterPage.class).dofilter();
+		extent.flush();
 	}
 	
 	@Test(priority=5)
 	public void getNames()
 	{
+		ExtentTest test= extent.createTest("Get Names of Hospitals");
 		name=page.getInstance(HospitalName.class).showHospitalName();
+		extent.flush();
 	}
 	
 	@Test(priority=6)
 	public void writeExcel()throws FileNotFoundException, IOException 
 	{
+		ExtentTest test= extent.createTest("Write in Excel");
 		page.getInstance(WriteData.class).doWrite(name);
+		extent.flush();
 	}
 
 		
 	@AfterTest
-	public void tearDown() {
+	public void tearDown() 
+	{
+		ExtentTest test= extent.createTest("Close Browser");
 		driver.quit();
-	
+		extent.flush();
 	}
 	
 
